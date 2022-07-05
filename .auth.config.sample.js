3617b0ba22926081c2cf36d71f123804e18200ce
Alec Dilanchian <alec.dilanchian@gmail.com>
2022-07-05 17:01:32 -0400

    Cleaing up contracts helpers

---
+++ .auth.config.sample.js
@@ -0,0 +1,13 @@
+module.exports = {
+  MUMBAI: {
+    BASE_TOKEN_URI: 'https://staging.tokens.buildspace.so/assets/',
+    NODE_URL: '', // alchemy endpoint
+    PRIVATE_KEY: '',
+  },
+  MATIC: {
+    BASE_TOKEN_URI: 'https://tokens.buildspace.so/assets/',
+    NODE_URL: '', // alchemy endpoint
+    PRIVATE_KEY: '', // contract pk
+    CONTRACT_ADDRESS: '0x3CD266509D127d0Eac42f4474F57D0526804b44e', // polygon contract address
+  },
+};
--- contracts/scripts/utils.js
+++ contracts/scripts/utils.js
@@ -31,110 +31,93 @@ async function main() {
   console.log('Token address:', token.address);
 }
 
-async function cancelTransaction(nonce) {
+// Helpers
+
+/**
+ * @param number nonce
+ * @param string gasPrice
+ * If a transaction is stuck, you can use this function to cancel that transaction as long as the gasPrice value is larger
+ */
+const cancelTransaction = async (nonce, gasPrice) => {
   const tx = await deployer.sendTransaction({
     to: config[NETWORK].CONTRACT_ADDRESS,
     value: 0,
     nonce: nonce,
-    gasPrice: ethers.utils.parseUnits('170', 'gwei'),
+    gasPrice: ethers.utils.parseUnits(gasPrice, 'gwei'),
     gasLimit: 100000,
   });
 
   console.log(tx);
-  // await tx.wait();
   console.log('done!');
-}
+};
 
-async function createCohort(cohortId, limit, merkleRoot) {
-  const tx = await contract.createCohort(cohortId, limit, merkleRoot, {
-    gasPrice: ethers.utils.parseUnits('100', 'gwei'),
-  });
-  console.log(tx);
-  await tx.wait();
-}
-
-async function fetchCohort(cohortId) {
+/**
+ * @param string cohortId
+ * Fetches the cohort on the contract and prints out the Limit number of the cohort
+ */
+const fetchCohort = async (cohortId) => {
   const tx = await contract.cohorts(cohortId);
   console.log(tx);
   console.log('LIMIT:', tx[0].toNumber());
-}
+};
 
-async function fetchUri(tokenId) {
+/**
+ * @param number tokenId
+ * Fetches the URI of the given tokenId
+ */
+const fetchUri = async (tokenId) => {
   const tx = await contract.tokenURI(tokenId);
   console.log(tx);
-}
+};
 
-async function updateMerkleRoot(cohortId, merkleRoot) {
+/**
+ * @param string cohortId
+ * @param string merkleRoot
+ * @param string gasPrice
+ * Updates the merkle root with new root value for a given cohort
+ */
+const updateMerkleRoot = async (cohortId, merkleRoot, gasPrice) => {
   const tx = await contract.setMerkleRoot(cohortId, merkleRoot, {
-    gasPrice: ethers.utils.parseUnits('50', 'gwei'),
+    gasPrice: ethers.utils.parseUnits(gasPrice, 'gwei'),
   });
   console.log(tx);
   await tx.wait();
-}
+};
 
-async function createCohort(cohort_id, limit) {
+/**
+ * @param string cohort_id
+ * @param number limit
+ * @param string gasPrice
+ * Creates a new cohort on the contract with an initial merkle root
+ */
+const createCohort = async (cohort_id, limit, gasPrice) => {
   const tx = await contract.createCohort(
     cohort_id,
     limit,
     '0x0000000000000000000000000000000000000000000000000000000000000000',
     {
-      gasPrice: ethers.utils.parseUnits('100', 'gwei'),
+      gasPrice: ethers.utils.parseUnits(gasPrice, 'gwei'),
       gasLimit: 100000,
     }
   );
   console.log(tx);
   await tx.wait();
-}
+};
 
-async function fetchOwner(address) {
-  const tx = await contract.claimed(
-    address,
-    'CH4f447780-07cf-408a-8f4c-253a8b4e8bae'
-  );
-  console.log(tx);
-  return tx;
-}
+/**
+ * @param string address
+ * @param string cohort_id
+ * Fetches the token number of a given cohort_id and address
+ */
+const fetchOwner = async (address, cohort_id) => {
+  const tx = await contract.claimed(address, cohort_id);
+  console.log(tx.toNumber());
+};
 
-//-- EXECUTION LAND --//
-
-// fetchOwner('0xE43973F95F344A1178CA661440E6A1F0d0a854B6')
-//   .then(() => process.exit(0))
-//   .catch((error) => {
-//     console.error(error);
-//   });
-
-// fetchUri(14629)
-//   .then(() => process.exit(0))
-//   .catch((error) => {
-//     console.error(error);
-//   });
-
-// const runAction = async () => {
-//   for (let i = 83854; i++; i < 83884) {
-//     await cancelTransaction(i);
-//     console.log('done');
-//   }
-// };
-
-// runAction();
-
-createCohort('CHa07ac83b-bc08-46b6-b561-abe05d5bda00', 1000)
+// Execution Land
+createCohort('CHaee9050e-fdb6-4094-9097-7f4fa00b8fd6', 1000, '100')
   .then(() => process.exit(0))
   .catch((error) => {
     console.error(error);
     process.exit(1);
   });
-
-// fetchCohort('CHe6f2f7a0-e85f-443e-95a6-28d143bc0d6d')
-//   .then(() => process.exit(0))
-//   .catch((error) => {
-//     console.error(error);
-//     process.exit(1);
-//   });
-
-// fetchUri('5579')
-// .then(() => process.exit(0))
-// .catch(error => {
-//   console.error(error);
-//   process.exit(1);
-// });
